e556a69769d505cc283a887fc46918b068bb979f
fjasper13 <fulgentiusjasper@gmail.com>
2019-04-01 14:12:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jasper/spplayer/Config.java
@@ -0,0 +1,7 @@
+package com.example.jasper.spplayer;
+
+public class Config {
+    public static final String CLIENT_ID = "P81pS7E4LB8vUEvQM98oMNsLe1wbYJqg";
+    public static final String CLIENT_SECRET = "4yLXoGbQTzOIgJxxU71kedrn6wMTIfVz";
+    public static final String API_URL = "https://api.soundcloud.com";
+}
---
+++ app/src/main/java/com/example/jasper/spplayer/SCService.java
@@ -0,0 +1,12 @@
+package com.example.jasper.spplayer;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface SCService {
+    @GET("/tracks?client_id=" + Config.CLIENT_ID)
+    Call<List<Track>> getRecentTracks (@Query("created_at")String date);
+}
